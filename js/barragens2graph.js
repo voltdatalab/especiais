cdaa5332b5b1ff0317fba899dbc5e9b951578391
voltdatalab <sergio@voltdata.info>
2015-11-19 22:46:26 -0200

    atualiza file barragens 2

---
+++ js/barragens2graph.js
@@ -0,0 +1,614 @@
+var margins = {
+        top: 12,
+        left: 30,
+        right: 200,
+        bottom: 50
+      };
+      var legendPanel = {
+        width: 200
+      };
+      var dataset = [{
+        data: [
+    {
+        "yValue": "MG",
+        "xValue": 3
+    },
+    {
+        "yValue": "PA",
+        "xValue": 3
+    },
+    {
+        "yValue": "SP",
+        "xValue": 0
+    },
+    {
+        "yValue": "MT",
+        "xValue": 0
+    },
+    {
+        "yValue": "RO",
+        "xValue": 0
+    },
+    {
+        "yValue": "BA",
+        "xValue": 0
+    },
+    {
+        "yValue": "MS",
+        "xValue": 2
+    },
+    {
+        "yValue": "GO",
+        "xValue": 0
+    },
+    {
+        "yValue": "AM",
+        "xValue": 10
+    },
+    {
+        "yValue": "SC",
+        "xValue": 0
+    },
+    {
+        "yValue": "AP",
+        "xValue": 0
+    },
+    {
+        "yValue": "TO",
+        "xValue": 0
+    },
+    {
+        "yValue": "RS",
+        "xValue": 0
+    },
+    {
+        "yValue": "SE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PR",
+        "xValue": 0
+    },
+    {
+        "yValue": "PE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PI",
+        "xValue": 0
+    },
+    {
+        "yValue": "RJ",
+        "xValue": 0
+    },
+    {
+        "yValue": "ES",
+        "xValue": 0
+    },
+    {
+        "yValue": "MA",
+        "xValue": 0
+    }
+],
+        name: 'Nota A'
+        }, 
+            {
+        data: [
+    {
+        "yValue": "MG",
+        "xValue": 3
+    },
+    {
+        "yValue": "PA",
+        "xValue": 3
+    },
+    {
+        "yValue": "SP",
+        "xValue": 0
+    },
+    {
+        "yValue": "MT",
+        "xValue": 0
+    },
+    {
+        "yValue": "RO",
+        "xValue": 0
+    },
+    {
+        "yValue": "BA",
+        "xValue": 0
+    },
+    {
+        "yValue": "MS",
+        "xValue": 0
+    },
+    {
+        "yValue": "GO",
+        "xValue": 0
+    },
+    {
+        "yValue": "AM",
+        "xValue": 1
+    },
+    {
+        "yValue": "SC",
+        "xValue": 1
+    },
+    {
+        "yValue": "AP",
+        "xValue": 0
+    },
+    {
+        "yValue": "TO",
+        "xValue": 0
+    },
+    {
+        "yValue": "RS",
+        "xValue": 0
+    },
+    {
+        "yValue": "SE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PR",
+        "xValue": 0
+    },
+    {
+        "yValue": "PE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PI",
+        "xValue": 0
+    },
+    {
+        "yValue": "RJ",
+        "xValue": 0
+    },
+    {
+        "yValue": "ES",
+        "xValue": 0
+    },
+    {
+        "yValue": "MA",
+        "xValue": 0
+    }
+],
+        name: 'Nota B'
+        }, 
+                     {
+        data: [
+    {
+        "yValue": "MG",
+        "xValue": 101
+    },
+    {
+        "yValue": "PA",
+        "xValue": 22
+    },
+    {
+        "yValue": "SP",
+        "xValue": 9
+    },
+    {
+        "yValue": "MT",
+        "xValue": 6
+    },
+    {
+        "yValue": "RO",
+        "xValue": 0
+    },
+    {
+        "yValue": "BA",
+        "xValue": 4
+    },
+    {
+        "yValue": "MS",
+        "xValue": 14
+    },
+    {
+        "yValue": "GO",
+        "xValue": 9
+    },
+    {
+        "yValue": "AM",
+        "xValue": 3
+    },
+    {
+        "yValue": "SC",
+        "xValue": 0
+    },
+    {
+        "yValue": "AP",
+        "xValue": 0
+    },
+    {
+        "yValue": "TO",
+        "xValue": 0
+    },
+    {
+        "yValue": "RS",
+        "xValue": 0
+    },
+    {
+        "yValue": "SE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PR",
+        "xValue": 1
+    },
+    {
+        "yValue": "PE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PI",
+        "xValue": 0
+    },
+    {
+        "yValue": "RJ",
+        "xValue": 0
+    },
+    {
+        "yValue": "ES",
+        "xValue": 0
+    },
+    {
+        "yValue": "MA",
+        "xValue": 0
+    }
+],
+        name: 'Nota C'
+        }, 
+                     {
+        data: [
+    {
+        "yValue": "MG",
+        "xValue": 70
+    },
+    {
+        "yValue": "PA",
+        "xValue": 21
+    },
+    {
+        "yValue": "SP",
+        "xValue": 19
+    },
+    {
+        "yValue": "MT",
+        "xValue": 17
+    },
+    {
+        "yValue": "RO",
+        "xValue": 2
+    },
+    {
+        "yValue": "BA",
+        "xValue": 5
+    },
+    {
+        "yValue": "MS",
+        "xValue": 1
+    },
+    {
+        "yValue": "GO",
+        "xValue": 4
+    },
+    {
+        "yValue": "AM",
+        "xValue": 0
+    },
+    {
+        "yValue": "SC",
+        "xValue": 3
+    },
+    {
+        "yValue": "AP",
+        "xValue": 3
+    },
+    {
+        "yValue": "TO",
+        "xValue": 3
+    },
+    {
+        "yValue": "RS",
+        "xValue": 3
+    },
+    {
+        "yValue": "SE",
+        "xValue": 2
+    },
+    {
+        "yValue": "PR",
+        "xValue": 0
+    },
+    {
+        "yValue": "PE",
+        "xValue": 1
+    },
+    {
+        "yValue": "PI",
+        "xValue": 1
+    },
+    {
+        "yValue": "RJ",
+        "xValue": 1
+    }, 
+    {
+        "yValue": "ES",
+        "xValue": 0
+    },
+    {
+        "yValue": "MA",
+        "xValue": 0
+    }
+],
+        name: 'Nota D'
+        }, 
+                     
+                     {
+        data: [
+    {
+        "yValue": "MG",
+        "xValue": 121
+    },
+    {
+        "yValue": "PA",
+        "xValue": 19
+    },
+    {
+        "yValue": "SP",
+        "xValue": 21
+    },
+    {
+        "yValue": "MT",
+        "xValue": 21
+    },
+    {
+        "yValue": "RO",
+        "xValue": 20
+    },
+    {
+        "yValue": "BA",
+        "xValue": 12
+    },
+    {
+        "yValue": "MS",
+        "xValue": 2
+    },
+    {
+        "yValue": "GO",
+        "xValue": 5
+    },
+    {
+        "yValue": "AM",
+        "xValue": 0
+    },
+    {
+        "yValue": "SC",
+        "xValue": 6
+    },
+    {
+        "yValue": "AP",
+        "xValue": 6
+    },
+    {
+        "yValue": "TO",
+        "xValue": 6
+    },
+    {
+        "yValue": "RS",
+        "xValue": 1
+    },
+    {
+        "yValue": "SE",
+        "xValue": 2
+    },
+    {
+        "yValue": "PR",
+        "xValue": 3
+    },
+    {
+        "yValue": "PE",
+        "xValue": 0
+    },
+    {
+        "yValue": "PI",
+        "xValue": 0
+    },
+    {
+        "yValue": "RJ",
+        "xValue": 0
+    },
+    {
+        "yValue": "ES",
+        "xValue": 1
+    },
+    {
+        "yValue": "MA",
+        "xValue": 1
+    }
+],
+        name: 'Nota E'
+      }];
+      var series = dataset.map(function (d) {
+        return d.name;
+      });
+      var numberOfRecords = 0;
+      var dataset = dataset.map(function (d) {
+        numberOfRecords = 0;
+        return d.data.map(function (o, i) {
+          numberOfRecords++;
+          return {
+            y: o.xValue,
+            x: o.yValue,
+            name: d.name
+          };
+        });
+      });
+      d3.select(window).on("resize", throttle);
+      var stack = d3.layout.stack();
+      stack(dataset);
+      var dataset = dataset.map(function (group) {
+        return group.map(function (d) {
+          // Invert the x and y values, and y0 becomes x0
+          return {
+            x: d.y + 2,
+            y: d.x,
+            x0: d.y0,
+            name:d.name
+          };
+        });
+      });
+      var yValues = dataset[0].map(function (d) {
+        return d.y;
+      });
+      var xMax = d3.max(dataset, function (group) {
+        return d3.max(group, function (d) {
+          return d.x + d.x0;
+        });
+      });
+      var tooltip = d3.select("graph")
+      .append('div')
+      .attr('id', 'tooltip')
+      .attr('class', 'hidden');
+      var width = window.innerWidth - margins.left - margins.right - legendPanel.width;
+      var barHeight = 20;
+      var height = (numberOfRecords * 30) - margins.top - margins.bottom;
+      var svg, xScale, yScale, rects;
+      
+      draw(width, height);
+      function draw(width, height) {
+      
+        svg = d3.select("graph")
+        .append('svg')
+        .attr('width', width + margins.left + margins.right)
+        .attr('height', height + margins.top + margins.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+      
+        xScale = d3.scale.linear()
+        .domain([0, xMax])
+        .range([0, width]);
+      
+        yScale = d3.scale.ordinal()
+        .domain(yValues)
+        .rangeRoundBands([0, height], .1);
+        var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom');
+        var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left');
+        var colours = d3.scale.category10()
+        .range(["#CF4647", "#FDD06B", "#A2AA61", "#297034", "#000"]);
+        var groups = svg.selectAll('g')
+        .data(dataset)
+        .enter()
+        .append('g')
+        .style('fill', function (d, i) {
+          return colours(i);
+        });
+        rects = groups.selectAll('rect')
+        .data(function (d) {
+          return d;
+        })
+        .enter()
+        .append('rect')
+        .attr('x', function (d) {
+          return xScale(d.x0);
+        })
+        .attr('y', function (d, i) {
+          return yScale(d.y);
+        })
+        .attr('height', function (d) {
+          return barHeight;
+        })
+        .attr('width', function (d) {
+          return xScale(d.x);
+        })
+        .on('mouseover', function (d) {
+      
+          var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
+          var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+      
+          d3.select('#tooltip')
+          .style('left', xPos + 'px')
+          .style('top', yPos + 'px')
+          .style('width', '250px')
+          .text(d.y+' tem '+d.x+' '+ ' barragens'+' com a '+d.name);
+      
+          d3.select('#tooltip').classed('hidden', false);
+        })
+        .on('mouseout', function () {
+          d3.select('#tooltip').classed('hidden', true);
+        });
+        svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+        svg.append('g')
+        .attr('class', 'axis')
+        .call(yAxis);
+        var legend = d3.select("graph")
+        .append('div')
+        .attr('id', 'legend')
+        .attr('class', 'row legend-row');
+        legend.append('div')
+        .attr('class', 'col-sm-4');
+        var legendColor1 = legend.append('div')
+        .attr('class', 'col-sm-4');
+        legendColor1.append('div')
+        .attr('class', 'legend-element')
+        .attr('style', 'background-color:'+colours(0));
+        legendColor1.append('div')
+        .attr('style', 'margin-left: 15px;')
+        .text(series[0]);
+        var legendColor2 = legend.append('div')
+        .attr('class', 'col-sm-4');
+        legendColor2.append('div')
+        .attr('class', 'legend-element')
+        .attr('style', 'background-color:'+colours(1));
+        legendColor2.append('div')
+        .attr('style', 'margin-left: 15px;')
+        .text(series[1]);
+        var legendColor3 = legend.append('div')
+        .attr('class', 'col-sm-4');
+        legendColor3.append('div')
+        .attr('class', 'legend-element')
+        .attr('style', 'background-color:'+colours(2));
+        legendColor3.append('div')
+        .attr('style', 'margin-left: 15px;')
+        .text(series[2]);
+        var legendColor4 = legend.append('div')
+        .attr('class', 'col-sm-4');
+        legendColor4.append('div')
+        .attr('class', 'legend-element')
+        .attr('style', 'background-color:'+colours(3));
+        legendColor4.append('div')
+        .attr('style', 'margin-left: 15px;')
+        .text(series[3]);
+        var legendColor5 = legend.append('div')
+        .attr('class', 'col-sm-4');
+        legendColor5.append('div')
+        .attr('class', 'legend-element')
+        .attr('style', 'background-color:'+colours(4));
+        legendColor5.append('div')
+        .attr('style', 'margin-left: 15px;')
+        .text(series[4]);
+      }
+      function redraw() {
+        width = window.innerWidth - margins.left - margins.right - legendPanel.width;
+        d3.select('svg').remove();
+        d3.select('#legend').remove();
+        draw(width,height);
+      }
+      var throttleTimer;
+      function throttle() {
+        window.clearTimeout(throttleTimer);
+        throttleTimer = window.setTimeout(function() {
+          redraw();
+        }, 200);
+      }
